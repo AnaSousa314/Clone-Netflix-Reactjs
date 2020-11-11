38bb55b8dcb76c0acc68b1941b88d73fa370ac08
Ana.Sousa <anasousa314@outlook.com>
2020-11-11 20:49:36 -0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
 export default () =>{
+
+  const [movieList, setMovieList] =useState([])
+
+  /* Quando a tela for Carregada, ele vai executar o useEffect */
+  useEffect(() => {
+      const loadAll = async ()=>{
+
+        // Pegando a lista total
+        let list = await Tmdb.getHomeList();
+          //console.log(list);
+
+          setMovieList(list);
+
+      }
+
+      loadAll();
+
+  }, []);
+
+
   return (
-    <div>
-      Olá Mundo
+    <div className="page">
+        
+        <section className="lists">
+          {movieList.map((item,key)=>{
+            return(
+            <div>
+              {item.title}
+            </div>)
+          })}
+        </section>
+
     </div>
   )
 }
